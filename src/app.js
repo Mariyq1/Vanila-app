bc921c47fab6d33291defb294ee1f0afc500ce0b
Mariyq1 <m4697597@gmail.com>
2022-10-08 19:48:58 +0200

    Showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,20 @@
+function displayTemperature(response){
+    console.log(response.data);
+    let temperatureElement= document.querySelector("#temperature");
+    temperatureElement.innerHTML =Math.round(response.data.main.temp);
+    let cityElement=document.querySelector("#city");
+    cityElement.innerHTML=response.data.name;
+    let descriptionElement=document.querySelector("#description");
+    descriptionElement.innerHTML=response.data.weather[0].description;
+      let humidityElement=document.querySelector("#humidity");
+    humidityElement.innerHTML=response.data.main.humidity;
+       let windElement=document.querySelector("#wind");
+    windElement.innerHTML=Math.round(response.data.wind.speed);
+}
+
+
+let apiKey= "3d5afe60db7c269fdb7213ce91827190";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+
+
+axios.get(apiUrl).then(displayTemperature);
