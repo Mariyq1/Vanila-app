4a013ef26173d8126353bb52aac2feae36510245
Mariyq1 <m4697597@gmail.com>
2022-10-09 18:26:38 +0200

    Link to C

--- src/app.js
+++ src/app.js
@@ -30,6 +30,8 @@ function displayTemperature(response){
     let windElement=document.querySelector("#wind");
     let dateElement= document.querySelector("#date");
     let iconElement= document.querySelector("#icon");
+    
+    celsiusTemperature=response.data.main.temp;
 
     temperatureElement.innerHTML =Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
@@ -53,8 +55,26 @@ function handleSubmit(event) {
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value);
 }
+function displayFahrenheitTemperature(event){
+    event.preventDefault();
+    let fahrenheitTemperature= (celsiusTemperature * 9) /5 + 32;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelsiusTemperature(event) {
+event.preventDefault();
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
-search("New York");
+}
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+search("New York");
